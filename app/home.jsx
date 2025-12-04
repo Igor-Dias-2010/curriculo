2e68feddc6f4ac33f7eeef93d2b0067606dd3b53
Igor da Silva Dias <rogigor1712@gmail.com>
2025-12-04 17:35:48 -0300

    curriculo V2.0

--- app/home.jsx
+++ app/home.jsx
@@ -1,16 +1,30 @@
 "use client"
 
 import Link from 'next/link'
-import { Mail, Phone, MapPin, MapPinCheck } from 'lucide-react'
+import { Mail, Phone, MapPin, MapPinCheck, Linkedin } from 'lucide-react'
 export default function Home() {
     return (
         <div>
             <h1>Igor da Silva Dias</h1>
-            <h2><Mail className='icon' /></h2>
-            <h2><Phone className='icon' /></h2>
-            <h2><MapPin className='icon' /></h2>
+            <div className='contact'>
+                <Mail className='icon' />
+                <Link href={'mailto:rogigor1712@gmail.com'}>rogigor1712@gmail.com</Link>
+            </div>
+            <div className='contact'>
+                <Phone className='icon' />
+                (48) 9186-6532
+            </div>
+            <div className='contact'>
+                <MapPin className='icon' />
+                Florianópolis/Santa Catarina
+            </div>
+            <div className='contact'>
+                <Linkedin className='icon' />
+                <Link href={'https://www.linkedin.com/in/igor-dias-4a0791356/'} target='_blank'>LinkedIn</Link>
 
+            </div>
             <hr />
+
         </div>
     )
 }
