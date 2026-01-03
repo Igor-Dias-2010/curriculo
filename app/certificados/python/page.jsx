ce1365820ae580ec541037b936b516e7e1d22da7
Igor da Silva Dias <rogigor1712@gmail.com>
2026-01-03 14:21:32 -0300

    Update page.jsx

--- app/certificados/python/page.jsx
+++ app/certificados/python/page.jsx
@@ -2,9 +2,12 @@
 
 export default function CertificadoPython(){
     return(
-        <div>
-            <h1>Curdo de Python</h1>
-            <p>RBCIP</p>
+        <div className="main">
+            <h1>Curso de Python</h1>
+            <p>RBCIP — 2025</p>
+            <figure>
+                <img src="" alt="" url='public/'>
+            </figure>
         </div>
     )
 }
