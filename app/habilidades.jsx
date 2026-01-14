71ac2fc753e6459507a74a1b233ca44d6b849e84
Igor da Silva Dias <rogigor1712@gmail.com>
2026-01-14 17:24:27 -0300

    deixei mais coisas em fonte de código

--- app/habilidades.jsx
+++ app/habilidades.jsx
@@ -6,7 +6,7 @@ export default function Habilidades() {
             <h2>Habilidades técnicas:</h2>
             <p>Domínio sólido de <span className="code">HTML</span>, <span className="code">CSS</span> e <span className="code">JavaScript</span>, capaz de desenvolver interfaces funcionais e responsivas.</p>
             <p>Experiência com <span className="code">Next.js</span>, incluindo criação de páginas, componentes reutilizáveis e aplicações funcionais.</p>
-            <p>Conhecimento em React Hooks (useState) para gerenciamento de estado.</p>
+            <p>Conhecimento em <span className="code">React Hooks</span> <span className="code">(useState)</span> para gerenciamento de estado.</p>
             <p>Uso frequente de <span className="code">Git</span> e <span className="code">GitHub</span> para versionamento e controle de código.</p>
             <p>Experiência com <span className="code">Linux (Zorin OS)</span> para desenvolvimento e gerenciamento de ambiente.</p>
             <p>Inglês intermediário, comunicação básica, ler documentação, escrever textos e entender inglês falado em velocidade moderada.</p>
